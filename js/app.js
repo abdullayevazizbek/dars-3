fac1ba7655d0e792c48b0d4b3aab911c64536efe
abdullayevazizbek <113992754+abdullayevazizbek@users.noreply.github.com>
2022-11-19 13:41:58 +0000

    brench

--- js/app.js
+++ js/app.js
@@ -2,9 +2,17 @@
 const mainBtn = document.querySelector('.main__btn')
 const mains = document.querySelector('.mains')
 const mainsBtn = document.querySelector('.mains__btn')
+const input = document.querySelectorAll('input')
+
 
 mainBtn.addEventListener('click', function () {
     mains.classList.add('block')
+    const email = document.querySelector('.emails')
+    const passwords = document.querySelector('.passwords')
+    const ConfirmPassword = document.querySelector('.ConfirmPasswords')
+    email.value = ''
+    passwords.value = ''
+    ConfirmPassword.value = ''
 })
 
 mainsBtn.addEventListener('click', function () {
@@ -23,23 +31,55 @@ submitBTn.addEventListener('click', function (event) {
     const email = document.querySelector('.emails').value
     const passwords = document.querySelector('.passwords').value
     const ConfirmPassword = document.querySelector('.ConfirmPasswords').value
-    const input = document.querySelectorAll('input').value
+    const emails = document.querySelector('.emails')
+    const passwordss = document.querySelector('.passwords')
+    const ConfirmPasswords = document.querySelector('.ConfirmPasswords')
+    const main = document.querySelector('.main')
     console.log(input);
-    if (email == "" || passwords == "" || ConfirmPassword === '') {
-        input.classList.add('red')
-        console.log('ERROR');
-    } else if (passwords === ConfirmPassword) {
+    const naramlizaData ={
+        email,
+        passwords,
+        ConfirmPassword,
+
+    }
+    for (const key in values) {
+        if (naramlizaData[key] === values[key]) {
+            errorList[key]= true            
+        }else{
+            errorList[key]= false
+        }
+    }
+    const errorList = {
+        email: false,
+        password: false,
+        ConfirmPassword:false,
+    }
+    const values = {
+        email: '',
+        password: '',
+        ConfirmPassword:'',
+    }
+    if (Object.values(errorList).every((item) => !item)) {
+        main.classList.add('mun')
         emailText.innerText = email
         passwordText.innerText = passwords
         ConfirmPasswordText.innerText = ConfirmPassword
-        submitBTn.addEventListener('click', function () {
-            mains.classList.remove('block')
-        })
+        mains.classList.remove('block')
+
+    } else {
+        for (const item in errorList) {
+            if (errorList[item]) {
+                document.querySelector(`${item}`).classList.add('red')                
+            }
+        }
     }
-
-
-    // email.value = ''
-    // passwords.value = ''
-    // ConfirmPassword.value = ''
-
+    
 })
+
+// if (passwords === ConfirmPassword) {
+
+//     emails.classList.add('red')
+//     passwordss.classList.add('red')
+//     ConfirmPasswords.classList.add('red')
+//     console.log('ERROR');
+// }
